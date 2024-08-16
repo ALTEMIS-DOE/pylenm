d8b5e373314ddf2102f2a4bde2a714c8ae7a8bc7
Satyarth Praveen <satyarth@lbl.gov>
2024-08-16 13:12:16 -0700

    Refactoring timeseries functions. Added imports. Cleaned functions. Added comments.

--- pylenm2/visualization/timeseries.py
+++ pylenm2/visualization/timeseries.py
@@ -1,8 +1,49 @@
-def plot_all_time_series_simple(self, analyte_name=None, start_date=None, end_date=None, title='Dataset: Time ranges', x_label='Well', y_label='Year',
-                            min_days=10, x_min_lim=-5, x_max_lim = 170, y_min_date='1988-01-01', y_max_date='2020-01-01', return_data=False, filter=False, col=None, equals=[]):
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+from dateutil.relativedelta import relativedelta
+
+from pyproj import Proj, Transformer
+
+import pylenm2
+from pylenm2.data import filters, fetchers, transformation
+from pylenm2.utils import constants as c
+
+import logging
+from pylenm2 import logger_config
+
+timeseries_logger = logger_config.setup_logging(
+    module_name=__name__,
+    # level=logging.INFO,
+    level=logging.DEBUG,
+    logfile_dir=c.LOGFILE_DIR,
+)
+
+
+def plot_all_time_series_simple(
+        # self, 
+        data_pylenm_dm,
+        analyte_name=None, 
+        start_date=None, 
+        end_date=None, 
+        title='Dataset: Time ranges', 
+        x_label='Well', 
+        y_label='Year',
+        min_days=10, 
+        x_min_lim=-5, 
+        x_max_lim = 170, 
+        y_min_date='1988-01-01', 
+        y_max_date='2020-01-01', 
+        return_data=False, 
+        filter=False, 
+        col=None, 
+        equals=[],
+    ):
     """Plots the start and end date of analyte readings for differnt locations/sensors/wells.
 
     Args:
+        data_pylenm_dm (pylenm2.PylenmDataModule): PylenmDataModule object containing the concentration and construction data.
         analyte_name (str, optional): analyte to examine. Defaults to None.
         start_date (str, optional): start date of horizontal time to show alignment. Defaults to None.
         end_date (str, optional): end date of horizontal time to show alignment.. Defaults to None.
@@ -19,20 +60,35 @@ def plot_all_time_series_simple(self, analyte_name=None, start_date=None, end_da
         col (str, optional): column to filter. Example: col='STATION_ID'. Defaults to None.
         equals (list, optional): values to filter col by. Examples: equals=['FAI001A', 'FAI001B']. Defaults to [].
     """
-    data = self.simplify_data()
+    # data = self.simplify_data()
+    data = filters.simplify_data(data=data_pylenm_dm)
+    
+    # Filtering
+    # -----------------
     if(filter):
-        filter_res = self.filter_by_column(data=self.construction_data, col=col, equals=equals)
+        filter_res = filters.filter_by_column(
+            data=data_pylenm_dm.construction_data, 
+            col=col, 
+            equals=equals,
+        )
         if('ERROR:' in str(filter_res)):
+            timeseries_logger.error("Ran into ERROR when calling filter_by_column()!")
             return filter_res
+        
         query_wells = list(data.STATION_ID.unique())
         filter_wells = list(filter_res.index.unique())
         intersect_wells = list(set(query_wells) & set(filter_wells))
         if(len(intersect_wells)<=0):
+            timeseries_logger.error('ERROR: No results for this query with the specifed filter parameters.')
             return 'ERROR: No results for this query with the specifed filter parameters.'
+        
         data = data[data['STATION_ID'].isin(intersect_wells)]
 
+    # Preparing data
+    # -----------------
     if(analyte_name!=None):
         data = data[data.ANALYTE_NAME == analyte_name]
+    
     wells = data.STATION_ID.unique()
     wells_dateRange=pd.DataFrame(columns=['STATION_ID','START_DATE','END_DATE'])
     for i in range(len(wells)):
@@ -43,13 +99,18 @@ def plot_all_time_series_simple(self, analyte_name=None, start_date=None, end_da
         wells_dateRange.loc[wells_dateRange.shape[0]]=[wellName,minDate,maxDate]
 
     wells_dateRange["RANGE"] = wells_dateRange.END_DATE - wells_dateRange.START_DATE
-    wells_dateRange.RANGE = wells_dateRange.RANGE.astype('timedelta64[D]').astype('int')
-    wells_dateRange = wells_dateRange[wells_dateRange.RANGE>min_days]
+    
+    # wells_dateRange.RANGE = wells_dateRange.RANGE.astype('timedelta64[D]').astype('int')
+    # wells_dateRange = wells_dateRange[wells_dateRange.RANGE>min_days]
+    wells_dateRange = wells_dateRange[wells_dateRange.RANGE.dt.days>min_days]
+    
     wells_dateRange.sort_values(by=["RANGE","END_DATE","START_DATE"], ascending = (False, False, True), inplace=True)
     wells_dateRange.reset_index(inplace=True)
     wells_dateRange.drop('index', axis=1, inplace=True)
     wells = np.array(wells_dateRange.STATION_ID)
 
+    # Plotting
+    # -----------------
     fig, ax = plt.subplots(1, 1, sharex=False,figsize=(20,6),dpi=300)
 
     ax.set_xticks(range(len(wells)))
@@ -71,20 +132,52 @@ def plot_all_time_series_simple(self, analyte_name=None, start_date=None, end_da
     ax.set_ylim([pd.to_datetime(y_min_date), pd.to_datetime(y_max_date)]) 
     ax.plot([], [], ' ', label="Time series with at least {} days".format(min_days))
     ax.legend()
+    
     if(analyte_name!=None):
         title = title + ' (' + analyte_name + ')'
     fig.suptitle(title, fontsize=20)
     for i in range(wells_dateRange.shape[0]):
         ax.vlines(i,wells_dateRange.loc[i,'START_DATE'],wells_dateRange.loc[i,'END_DATE'],colors='k')
+    
     if(return_data):
         return wells_dateRange
 
-def plot_all_time_series(self, analyte_name=None, title='Dataset: Time ranges', x_label='Well', y_label='Year', x_label_size=8, marker_size=30,
-                        min_days=10, x_min_lim=None, x_max_lim=None, y_min_date=None, y_max_date=None, sort_by_distance=True, source_coordinate=[436642.70,3681927.09], log_transform=False, cmap=mpl.cm.rainbow, 
-                        drop_cols=[], return_data=False, filter=False, col=None, equals=[], cbar_min=None, cbar_max=None, reverse_y_axis=False, fontsize = 20, figsize=(20,6), dpi=300, y_2nd_label=None):
+
+def plot_all_time_series(
+        # self, 
+        data_pylenm_dm,
+        analyte_name=None, 
+        title='Dataset: Time ranges', 
+        x_label='Well', 
+        y_label='Year', 
+        x_label_size=8, 
+        marker_size=30,
+        min_days=10, 
+        x_min_lim=None, 
+        x_max_lim=None, 
+        y_min_date=None, 
+        y_max_date=None, 
+        sort_by_distance=True, 
+        source_coordinate=[436642.70,3681927.09], 
+        log_transform=False, 
+        cmap=mpl.cm.rainbow, 
+        drop_cols=[], 
+        return_data=False, 
+        filter=False, 
+        col=None, 
+        equals=[], 
+        cbar_min=None, 
+        cbar_max=None, 
+        reverse_y_axis=False, 
+        fontsize = 20, 
+        figsize=(20,6), 
+        dpi=300, 
+        y_2nd_label=None,
+    ):      # TODO: Getting error in the notebook!
     """Plots the start and end date of analyte readings for differnt locations/sensors/wells with colored concentration reading.
 
     Args:
+        data_pylenm_dm (pylenm2.PylenmDataModule): PylenmDataModule object containing the concentration and construction data.
         analyte_name (str, optional): analyte to examine. Defaults to None.
         title (str, optional): plot title. Defaults to 'Dataset: Time ranges'.
         x_label (str, optional): x axis label. Defaults to 'Well'.
@@ -113,52 +206,90 @@ def plot_all_time_series(self, analyte_name=None, title='Dataset: Time ranges', 
         dpi (int, optional): DPI of figure. Defaults to 300.
         y_2nd_label (str, optional): color bar label manual override. Defaults to None.
     """
-    dt = self.getCleanData([analyte_name])
-    dt = dt[analyte_name]
-    try:
-        if(filter):
-            filter_res = self.filter_by_column(data=self.get_Construction_Data(), col=col, equals=equals)
-            if('ERROR:' in str(filter_res)):
-                return filter_res
-            query_wells = list(dt.columns.unique())
-            filter_wells = list(filter_res.index.unique())
-            intersect_wells = list(set(query_wells) & set(filter_wells) & set(dt.columns))
-            if(len(intersect_wells)<=0):
-                return 'ERROR: No results for this query with the specifed filter parameters.'
-            dt = dt[intersect_wells]
     
-        well_info = self.get_Construction_Data()
-        shared_wells = list(set(well_info.index) & set(dt.columns))
-        dt = dt[shared_wells]
-        well_info = well_info.T[shared_wells]
-        dt = dt.reindex(sorted(dt.columns), axis=1)
-        well_info = well_info.reindex(sorted(well_info.columns), axis=1)
-        well_info = well_info.T
-        transformer = Transformer.from_crs("epsg:4326", "epsg:26917") # Latitude/Longitude to UTM
-        UTM_x, UTM_y = transformer.transform(well_info.LATITUDE, well_info.LONGITUDE)
-        X = np.vstack((UTM_x,UTM_y)).T
-        well_info = pd.DataFrame(X, index=list(well_info.index),columns=['Easting', 'Northing'])
-        well_info = self.add_dist_to_source(well_info, source_coordinate=source_coordinate)
-        if(sort_by_distance):
-            well_info.sort_values(by=['dist_to_source'], ascending = True, inplace=True)
-        dt = dt[well_info.index]
-    except:
-        pass
+    # Get clean data
+    # dt = self.getCleanData([analyte_name])
+    dt = fetchers.getCleanData(
+        data_pylenm_dm, 
+        analytes=[] if analyte_name is None else [analyte_name],
+    )
+    dt = dt[analyte_name]
+    
+    # try:
+    # Filter data (if necessary)
+    if(filter):
+        filter_res = filters.filter_by_column(
+            data=data_pylenm_dm.get_construction_data(), 
+            col=col, 
+            equals=equals,
+        )
+        if('ERROR:' in str(filter_res)):
+            timeseries_logger.error("Ran into ERROR when calling filter_by_column()!")
+            return filter_res
+        
+        query_wells = list(dt.columns.unique())
+        filter_wells = list(filter_res.index.unique())
+        intersect_wells = list(set(query_wells) & set(filter_wells) & set(dt.columns))
+        if(len(intersect_wells)<=0):
+            timeseries_logger.error('ERROR: No results for this query with the specifed filter parameters.')
+            return 'ERROR: No results for this query with the specifed filter parameters.'
+        dt = dt[intersect_wells]
 
+    # Get well information
+    well_info = data_pylenm_dm.get_construction_data()
+    shared_wells = list(set(well_info.index) & set(dt.columns))
+    dt = dt[shared_wells]
+    well_info = well_info.T[shared_wells]
+    dt = dt.reindex(sorted(dt.columns), axis=1)
+    well_info = well_info.reindex(sorted(well_info.columns), axis=1)
+    well_info = well_info.T
+    
+    # Add distance to source
+    transformer = Transformer.from_crs("epsg:4326", "epsg:26917") # Latitude/Longitude to UTM
+    UTM_x, UTM_y = transformer.transform(well_info.LATITUDE, well_info.LONGITUDE)
+    X = np.vstack((UTM_x,UTM_y)).T
+    well_info = pd.DataFrame(X, index=list(well_info.index),columns=['Easting', 'Northing'])
+    well_info = transformation.add_dist_to_source(
+        XX=well_info, 
+        source_coordinate=source_coordinate,
+    )
+
+    # Sort by distance (if necessary)
+    if(sort_by_distance):
+        well_info.sort_values(
+            by=['dist_to_source'], 
+            ascending=True, 
+            inplace=True,
+        )
+    
+    dt = dt[well_info.index]
+
+    # except Exception as e:
+    #     timeseries_logger.error(f"Error occurred but ignoring the error - {e}")
+
+    # Iterpolate/drop data
     dt = dt.interpolate()
     dt = dt.drop(drop_cols, axis=1) # DROP BAD ONES 
     
+    # Take log transform of the data (if necessary)
     if(log_transform):
         dt[dt <= 0] = 0.00000001
         dt = np.log10(dt)
+    
+    # Plot data
     wells = dt.columns
-    if(cbar_min==None):
-        cbar_min = dt.min().min()
-    if(cbar_max==None):
-        cbar_max = dt.max().max() 
+    # if(cbar_min==None):
+    #     cbar_min = dt.min().min()
+    # if(cbar_max==None):
+    #     cbar_max = dt.max().max()
+    cbar_min = dt.min().min() if cbar_min is None else cbar_min 
+    cbar_max = dt.max().max() if cbar_max is None else cbar_max
     norm = mpl.colors.Normalize(vmin=cbar_min, vmax=cbar_max)
 
-    fig, ax = plt.subplots(1, 2, sharex=False, figsize=figsize, dpi=dpi, gridspec_kw={'width_ratios': [40, 1]})
+    fig, ax = plt.subplots(
+        1, 2, sharex=False, figsize=figsize, dpi=dpi, 
+        gridspec_kw={'width_ratios': [40, 1]},
+    )
     ax[0].set_xticks(range(len(wells)))
     ax[0].set_xticklabels(wells, rotation='vertical', fontsize=x_label_size)
 
@@ -168,44 +299,56 @@ def plot_all_time_series(self, analyte_name=None, title='Dataset: Time ranges', 
         length = len(list(dt[col].loc[curr_start:curr_end].index))
         color_vals = list(dt[col].loc[curr_start:curr_end])
         color_vals = [cmap(norm((x))) for x in color_vals]
+        
         x = col
         ys = list(dt[col].loc[curr_start:curr_end].index)
         ax[0].scatter([str(x)]*length, ys, c=color_vals, marker='o',lw=0,s=marker_size, alpha=0.75)
 
+    # Set plot labels
     x_label = x_label + ' (count: ' + str(dt.shape[1])+ ')'
     ax[0].set_xlabel(x_label, fontsize=fontsize)
     ax[0].set_ylabel(y_label, fontsize=fontsize)
-    if(x_min_lim==None):
-        x_min_lim = -5
-    if(x_max_lim==None):
-        x_max_lim = len(wells)+5
+    
+    # Set plot limits
+    # if(x_min_lim==None):
+    #     x_min_lim = -5
+    # if(x_max_lim==None):
+    #     x_max_lim = len(wells)+5
+    x_min_lim = -5 if x_min_lim is None else x_min_lim
+    x_max_lim = len(wells)+5 if x_max_lim is None else x_max_lim
     ax[0].set_xlim([x_min_lim, x_max_lim])
-    if(y_min_date==None):
+    
+    if (y_min_date==None):
         y_min_date = dt.index.min() + relativedelta(years=-1)
-    if(y_max_date==None):
+    if (y_max_date==None):
         y_max_date = dt.index.max() + relativedelta(years=1)
     ax[0].set_ylim([pd.to_datetime(y_min_date), pd.to_datetime(y_max_date)]) 
     ax[0].plot([], [], ' ', label="Time series with at least {} days".format(min_days))
-    ax[0].set_facecolor((0, 0, 0,0.1))
+    ax[0].set_facecolor((0, 0, 0, 0.1))
             
-    # COLORBAR
-    label_cb = "Concentration ({})".format(self.get_unit(analyte_name))
+    # Add colorbar
+    label_cb = "Concentration ({})".format(data_pylenm_dm.get_unit(analyte_name))
     if(log_transform):
         label_cb = "Log " + label_cb
-    cbar = fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
-            cax=ax[1], orientation='vertical')
+    cbar = fig.colorbar(
+        mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
+        cax=ax[1], 
+        orientation='vertical',
+    )
     if(y_2nd_label!=None):
         label_cb = y_2nd_label
-    cbar.set_label(label=label_cb,size=fontsize)
+    cbar.set_label(label=label_cb, size=fontsize)
 
     ax[0].tick_params(axis='y', labelsize=fontsize)
     cbar.ax.tick_params(labelsize=fontsize)
     plt.tight_layout()
     if(reverse_y_axis):
         ax[0].invert_yaxis()
+    
     if(analyte_name!=None):
         title = title + ' (' + analyte_name + ')'
     fig.suptitle(title, fontsize=fontsize, y=1.05)
+    
+    # Return data if necessary
     if(return_data):
         return dt
-
