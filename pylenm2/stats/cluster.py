1e39acb9f6c6e79e2d7777a9846d0e901b6026e0
Satyarth Praveen <satyarth@lbl.gov>
2024-09-08 21:33:08 -0700

    Refactored cluster_data(). Did not refactor cluster_data_OLD()

--- pylenm2/stats/cluster.py
+++ pylenm2/stats/cluster.py
@@ -1,3 +1,23 @@
+import pandas as pd
+import matplotlib.dates as mdates
+from sklearn.cluster import KMeans
+from matplotlib import pyplot as plt
+from matplotlib.dates import date2num
+
+from pylenm2.data import filters
+from pylenm2.utils import constants as c
+
+import logging
+from pylenm2 import logger_config
+
+cluster_logger = logger_config.setup_logging(
+    module_name=__name__,
+    # level=logging.INFO,
+    level=logging.DEBUG,
+    logfile_dir=c.LOGFILE_DIR,
+)
+
+
 def __cluster_data_OLD(self, data, n_clusters=4, log_transform=False, filter=False, filter_well_by=['D'], return_clusters=False):
     if(filter):
         res_wells = self.filter_wells(filter_well_by)
@@ -34,10 +54,24 @@ def __cluster_data_OLD(self, data, n_clusters=4, log_transform=False, filter=Fal
             gps_color = pd.merge(self.get_Construction_Data(), color_df, on=['STATION_ID'])
             return gps_color
 
-def cluster_data(self, data, analyte_name=["ANALYTE_NAME"], n_clusters=4, filter=False, col=None, equals=[], year_interval=5, y_label = 'Concentration', return_clusters=False ):
+
+def cluster_data(
+        # self, 
+        data_pylenm_dm,
+        data, 
+        analyte_name=["ANALYTE_NAME"], 
+        n_clusters=4, 
+        filter=False, 
+        col=None, 
+        equals=[], 
+        year_interval=5, 
+        y_label = 'Concentration', 
+        return_clusters=False,
+    ):
     """Clusters time series concentration data using kmeans algorithm and plots it.
 
     Args:
+        data_pylenm_dm (pylenm2.PylenmDataModule): PylenmDataModule object containing the concentration and construction data.
         data (pd.DataFrame): data to be used in clustering.
         analyte_name (list, optional): analytes to use to cluster. Defaults to ["ANALYTE_NAME"].
         n_clusters (int, optional): number of clusters for kmeans. Defaults to 4.
@@ -48,30 +82,56 @@ def cluster_data(self, data, analyte_name=["ANALYTE_NAME"], n_clusters=4, filter
         y_label (str, optional): y axis label. Defaults to 'Concentration'.
         return_clusters (bool, optional): flag to return cluster assignemnt. Defaults to False.
     """
+    
     data = data.copy()
+
+    # Filter data (if specified)
     if(filter):
-        filter_res = self.filter_by_column(data=self.get_Construction_Data(), col=col, equals=equals)
-        if('ERROR:' in str(filter_res)):
-            return filter_res
+        # filter_res = self.filter_by_column(data=self.get_Construction_Data(), col=col, equals=equals)
+        filter_res = filters.filter_by_column(
+            data=data_pylenm_dm.get_construction_data(), 
+            col=col, 
+            equals=equals,
+        )
+
+        # if('ERROR:' in str(filter_res)):
+        #     return filter_res
+        if filter_res is None:
+            return None
+        
         query_wells = list(data.columns)
         filter_wells = list(filter_res.index.unique())
         intersect_wells = list(set(query_wells) & set(filter_wells))
-        print(intersect_wells)
-        if(len(intersect_wells)<=0):
-            return 'ERROR: No results for this query with the specifed filter parameters.'
+        
+        cluster_logger.info(f"{intersect_wells = }")
+        cluster_logger.info(f"{len(intersect_wells) = }")
+        
+        if len(intersect_wells) <= 0:
+            # return 'ERROR: No results for this query with the specifed filter parameters.'
+            cluster_logger.error("ERROR: No results for this query with the specifed filter parameters.")
+            return None
+
         data = data[intersect_wells]
+    
     data.index = date2num(data.index)
     temp = data.T
-    k_Means = KMeans(n_clusters=n_clusters, random_state=43)
+
+    # KMeans clustering
+    k_Means = KMeans(n_init="auto", n_clusters=n_clusters, random_state=43)
     km = k_Means.fit(temp)
     predict = km.predict(temp)
+    
     temp['predicted'] = km.labels_
-    colors = ['red', 'blue', 'orange', 'purple', 'green', 'pink', 'black', 'cadetblue', 'lightgreen','beige']
+    colors = [
+        'red', 'blue', 'orange', 'purple', 'green', 'pink', 
+        'black', 'cadetblue', 'lightgreen','beige',
+    ]
     temp['color'] = temp['predicted'].map(lambda p: colors[p])
     
+    # Generate plots
     fig, ax = plt.subplots(figsize=(10,10), dpi=100)
     ax.minorticks_off()
-    ax = plt.axes()
+    # ax = plt.axes()
 
     color = temp['color']
     for x in range(temp.shape[0]):
@@ -81,24 +141,37 @@ def cluster_data(self, data, analyte_name=["ANALYTE_NAME"], n_clusters=4, filter
     years = mdates.YearLocator(year_interval)  # every year
     months = mdates.MonthLocator()  # every month
     yearsFmt = mdates.DateFormatter('%Y') 
+    
     ax = plt.gca()
     ax.xaxis.set_major_locator(years)
     ax.xaxis.set_major_locator(years)
     ax.xaxis.set_major_formatter(yearsFmt)
     ax.autoscale_view()
+    
     ax.set_xlabel("Years", fontsize=20)
     plt.xticks(fontsize=20)
+    
     ax.set_ylabel(y_label, fontsize=20)
     plt.yticks(fontsize=20)
-    ax.set_title("{}: {} clusters".format(analyte_name, n_clusters), fontsize=20)
+    
+    # ax.set_title("{}: {} clusters".format(analyte_name, n_clusters), fontsize=20)
+    ax.set_title(f"{analyte_name}: {n_clusters} clusters", fontsize=20)
+    
+    # Return clusters (if specified)
     if(return_clusters):
         color_df = pd.DataFrame(temp['color'])
         color_df['STATION_ID'] = color_df.index
-        if(self.get_Construction_Data==None):
-            print('You need to set the GPS data first using the setConstructionData function.')
+        
+        if data_pylenm_dm.get_construction_data() is None:
+            cluster_logger.error("You need to set the GPS data first using the setConstructionData function.")
             return None
         else:
-            gps_color = pd.merge(self.get_Construction_Data(), color_df, on=['STATION_ID'])
+            gps_color = pd.merge(
+                data_pylenm_dm.get_construction_data(), 
+                color_df, 
+                on=['STATION_ID'],
+            )
             return gps_color
+    
     # if(return_data):
     #     return color, temp
