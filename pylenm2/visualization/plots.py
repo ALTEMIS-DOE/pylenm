47aac57dca931401c4470376e587775c43e693f7
Satyarth Praveen <satyarth@lbl.gov>
2024-09-16 19:18:22 +0530

    Refactoring, fixing the plots. Updating error logs and returns. Handling some error cases.

--- pylenm2/visualization/plots.py
+++ pylenm2/visualization/plots.py
@@ -1,11 +1,19 @@
 import os
+import scipy
+import random
+import itertools
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
+import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from matplotlib.dates import date2num, num2date
+
 from supersmoother import SuperSmoother
 
 from pylenm2.data import filters
+from pylenm2.stats import preprocess
 from pylenm2.utils import constants as c
 
 import logging
@@ -96,16 +104,20 @@ def plot_data(
     """
 
     # Gets appropriate data (well_name and analyte_name)
-    query = filters.query_data(
+    query_raw = filters.query_data(
         data_pylenm_dm=data_pylenm_dm, 
         well_name=well_name, 
         analyte_name=analyte_name,
     )       # TODO: Handle better for empty values.
-    query = filters.simplify_data(data=query)
 
+    query = filters.simplify_data(data=query_raw)
+    
     # Check if the query returned any results
-    if(type(query)==int and query == 0):
-        return f'No results found for {well_name} and {analyte_name}'
+    # if(type(query)==int and query == 0):
+    if query is None:
+        plots_logger.error(f"No results found for {well_name} and {analyte_name}")
+        return None
+    
     else:
         # Filter the data by the specified column and values (if necessary)
         if(filter):
@@ -141,13 +153,20 @@ def plot_data(
 
         # Create and fit supersmoother model
         model = SuperSmoother(alpha=alpha)
-        model.fit(x_RR, y_data)
+        try:
+            model.fit(x_RR, y_data)
+
+            # Get smoothed predictions, residuals and outliers from the model.
+            y_pred = model.predict(x_RR)
         
-        # Get smoothed predictions, residuals and outliers from the model.
-        y_pred = model.predict(x_RR)
+        except Exception as e:
+            plots_logger.error(e)
+            return None
+
         r = model.cv_residuals()
-        out = abs(r) > 2.2*np.std(r)
+        out = abs(r) > (2.2 * np.std(r))
         out_x = x_data[out]
+            
         out_y = y_data[out]
 
         # Plot data
@@ -171,25 +190,31 @@ def plot_data(
         ax.set_title(str(well_name) + ' - ' + analyte_name, fontweight='bold')
         ttl = ax.title
         ttl.set_position([.5, 1.05])
+        
         if(y_label==''):    
             if(log_transform):
                 ax.set_ylabel('log-Concentration (' + unit + ')')
             else:
                 ax.set_ylabel('Concentration (' + unit + ')')
+        
         else:
             ax.set_ylabel(y_label)
+        
         ax.set_xlabel(x_label)
         small_fontSize = 15
         large_fontSize = 20
+        
         plt.rc('axes', titlesize=large_fontSize)
         plt.rc('axes', labelsize=large_fontSize)
         plt.rc('legend', fontsize=small_fontSize)
         plt.rc('xtick', labelsize=small_fontSize)
         plt.rc('ytick', labelsize=small_fontSize) 
+        
         ax.plot(x_data, y_data, ls='', marker='o', ms=5, color='black', alpha=1)
         ax.plot(x_data, y_pred, ls='-', marker='', ms=5, lw=2, color='blue', alpha=0.5, label="Super Smoother")
         ax.plot(out_x , out_y, ls='', marker='o', ms=5, color='red', alpha=1, label="Outliers")
         ax.legend(bbox_to_anchor=(1.04, 1), loc='upper left', borderaxespad=0.)
+        
         props = dict(boxstyle='round', facecolor='grey', alpha=0.15)       
         ax.text(1.05, 0.85, 'Samples: {}'.format(nu), transform=ax.transAxes, 
                 fontsize=small_fontSize,
@@ -215,48 +240,84 @@ def plot_data(
         plt.cla()
         plt.close()
 
+        # Returning a non-None value to mark a successful execution
+        return 200
 
-def plot_all_data(self, log_transform=True, alpha=0, year_interval=2, plot_inline=True, save_dir='plot_data'):
+
+def plot_all_data(
+        # self, 
+        data_pylenm_dm,
+        log_transform=True, 
+        alpha=0, 
+        year_interval=2, 
+        plot_inline=True, 
+        save_dir='plot_data',
+    ):
     """Plot concentrations over time for every well and analyte with a smoothed curve on interpolated data points.
 
     Args:
+        data_pylenm_dm (pylenm2.PylenmDataModule): PylenmDataModule object containing the concentration and construction data.
         log_transform (bool, optional): choose whether or not the data should be transformed to log base 10 values. Defaults to True.
         alpha (int, optional): alue between 0 and 10 for line smoothing. Defaults to 0.
         plot_inline (bool, optional): choose whether or not to show plot inline. Defaults to True.
         year_interval (int, optional): plot by how many years to appear in the axis e.g.(1 = every year, 5 = every 5 years, ...). Defaults to 2.
         save_dir (str, optional): name of the directory you want to save the plot to. Defaults to 'plot_data'.
     """
+    
     analytes = ['TRITIUM','URANIUM-238','IODINE-129','SPECIFIC CONDUCTANCE', 'PH', 'DEPTH_TO_WATER']
-    wells = np.array(self.data.STATION_ID.values)
+    
+    wells = np.array(data_pylenm_dm.data.STATION_ID.values)
     wells = np.unique(wells)
+
+    wells_analytes_iter = list(itertools.product(wells, analytes))
+    
     success = 0
     errors = 0
-    for well in wells:
-        for analyte in analytes:
-            plot = self.plot_data(well, analyte, 
-                                log_transform=log_transform, 
-                                alpha=alpha, 
-                                year_interval=year_interval,
-                                plot_inline=plot_inline,
-                                save_dir=save_dir)
-            if 'ERROR:' in str(plot):
-                errors = errors + 1
-            else:
-                success = success + 1
-    print("Success: ", success)
-    print("Errors: ", errors)
+    
+    # for well in wells:
+    #     for analyte in analytes:
+    for well, analyte in tqdm(wells_analytes_iter, desc="Well Analyte Pair", total=(len(analytes)*len(wells))):
+        plot = plot_data(
+            data_pylenm_dm=data_pylenm_dm,
+            well_name=well, 
+            analyte_name=analyte, 
+            log_transform=log_transform, 
+            alpha=alpha, 
+            year_interval=year_interval, 
+            plot_inline=plot_inline, 
+            save_dir=save_dir,
+        )
+        
+        if plot is None:
+            errors = errors + 1
+        else:
+            success = success + 1
+
+    plots_logger.info(f"Success: {success}")
+    plots_logger.info(f"Errors: {errors}")
 
         
-def plot_MCL(self, well_name, analyte_name, year_interval=5, save_dir='plot_MCL'):
+def plot_MCL(
+        # self, 
+        data_pylenm_dm,
+        well_name, 
+        analyte_name, 
+        year_interval=5, 
+        save_dir='plot_MCL',
+    ):
     """Plots the linear regression line of data given the analyte_name and well_name. The plot includes the prediction where the line of best fit intersects with the Maximum Concentration Limit (MCL).
 
     Args:
+        data_pylenm_dm (pylenm2.PylenmDataModule): PylenmDataModule object containing the concentration and construction data.
         well_name (str): ame of the well to be processed
         analyte_name (str): name of the analyte to be processed
         year_interval (int, optional): lot by how many years to appear in the axis e.g.(1 = every year, 5 = every 5 years, ...). Defaults to 5.
         save_dir (str, optional): name of the directory you want to save the plot to. Defaults to 'plot_MCL'.
     """
-    data = self.data
+    
+    # data = self.data
+    data = data_pylenm_dm.data
+    
     # finds the intersection point of 2 lines given the slopes and y-intercepts
     def line_intersect(m1, b1, m2, b2):
         if m1 == m2:
@@ -267,18 +328,28 @@ def plot_MCL(self, well_name, analyte_name, year_interval=5, save_dir='plot_MCL'
         return x,y
 
     # Gets appropriate data (well_name and analyte_name)
-    query = self.query_data(well_name, analyte_name)
+    # query = self.query_data(well_name, analyte_name)
+    query = filters.query_data(
+        data_pylenm_dm=data_pylenm_dm,
+        well_name=well_name, 
+        analyte_name=analyte_name,
+    )
 
-    if(type(query)==int and query == 0):
-        return 'No results found for {} and {}'.format(well_name, analyte_name)
-    else:   
-
-        test = query.groupby(['COLLECTION_DATE']).mean()
+    # if(type(query)==int and query == 0):
+    if isinstance(query, int) and query==0:
+        plots_logger.info(
+            f"No results found for {well_name} and {analyte_name}"
+        )
+        return None
+    
+    else:
+        test = query.groupby(['COLLECTION_DATE'])['RESULT'].mean()
         test.index = pd.to_datetime(test.index)
 
         x = date2num(test.index)
-        y = np.log10(test.RESULT)
-        ylabel = 'log-Concentration (' + self.get_unit(analyte_name) + ')'
+        # y = np.log10(test.RESULT)
+        y = np.log10(test)
+        ylabel = 'log-Concentration (' + data_pylenm_dm.get_unit(analyte_name) + ')'
         y = y.rename(ylabel)
 
         p, cov = np.polyfit(x, y, 1, cov=True)  # parameters and covariance from of the fit of 1-D polynom.
@@ -289,7 +360,8 @@ def plot_MCL(self, well_name, analyte_name, year_interval=5, save_dir='plot_MCL'
         f = np.poly1d(p)
 
         try:
-            MCL = self.get_MCL(analyte_name)
+            # MCL = self.get_MCL(analyte_name)
+            MCL = preprocess.get_MCL(analyte_name)
             m1, b1 = f # line of best fit
             m2, b2 = 0, MCL # MCL constant
 
@@ -301,6 +373,7 @@ def plot_MCL(self, well_name, analyte_name, year_interval=5, save_dir='plot_MCL'
             list_slopes = []
             list_intercepts = []
             random.seed(50)
+            
             for _ in range(80):
                 sampled_data = [ random.choice(data) for _ in range(n) ]
                 x_s, y_s = zip(*sampled_data)
@@ -308,12 +381,14 @@ def plot_MCL(self, well_name, analyte_name, year_interval=5, save_dir='plot_MCL'
                 y_s = np.array(y_s)
 
                 m_s, b_s, r, p, err = scipy.stats.linregress(x_s,y_s)
-                ymodel = m_s*x_s + b_s
+                ymodel = (m_s * x_s) + b_s
+                
                 list_slopes.append(m_s)
                 list_intercepts.append(b_s)
 
             max_index = list_slopes.index(max(list_slopes))
             min_index = list_slopes.index(min(list_slopes))
+            
             intersection_left = line_intersect(list_slopes[min_index], list_intercepts[min_index], m2, b2)
             intersection_right = line_intersect(list_slopes[max_index], list_intercepts[max_index], m2, b2)
             ##
@@ -327,6 +402,10 @@ def plot_MCL(self, well_name, analyte_name, year_interval=5, save_dir='plot_MCL'
             months = mdates.MonthLocator()  # every month
             yearsFmt = mdates.DateFormatter('%Y') 
 
+            for label in ax.get_xticklabels():
+                label.set_rotation(30)
+                label.set_horizontalalignment('center')
+
             ax.xaxis.set_major_locator(years)
             ax = plt.gca()
             ax.xaxis.set_major_locator(years)
@@ -335,6 +414,7 @@ def plot_MCL(self, well_name, analyte_name, year_interval=5, save_dir='plot_MCL'
             ax.grid(True, alpha=0.4)
             small_fontSize = 15
             large_fontSize = 20
+            
             plt.rc('axes', titlesize=large_fontSize)
             plt.rc('axes', labelsize=large_fontSize)
             plt.rc('legend', fontsize=small_fontSize)
@@ -342,7 +422,7 @@ def plot_MCL(self, well_name, analyte_name, year_interval=5, save_dir='plot_MCL'
             plt.rc('ytick', labelsize=small_fontSize)
 
             ax.set_xlabel('Years')
-            ax.set_ylabel('log-Concentration (' + self.get_unit(analyte_name) + ')')
+            ax.set_ylabel('log-Concentration (' + data_pylenm_dm.get_unit(analyte_name) + ')')
 
             if(intersection[0] < min(x)):
                 temp = intersection_left
@@ -350,6 +430,7 @@ def plot_MCL(self, well_name, analyte_name, year_interval=5, save_dir='plot_MCL'
                 intersection_right = temp
                 ax.set_ylim([0, max(y)+1])
                 ax.set_xlim([intersection_left[0]-1000, max(x)+1000])
+            
             elif(intersection[0] < max(x) and intersection[0] > min(x)):
                 ax.set_ylim([0, max(y)+1])
                 ax.set_xlim(min(x)-1000, max(x)+1000)
@@ -358,7 +439,10 @@ def plot_MCL(self, well_name, analyte_name, year_interval=5, save_dir='plot_MCL'
                 ax.set_ylim([0, max(y)+1])
                 ax.set_xlim([min(x)-1000, intersection_right[0]+1000])
 
-            ax = sns.regplot(x, y, logx=True, truncate=False, seed=42, n_boot=1000, ci=95) # Line of best fit
+            ax = sns.regplot(
+                x=x, y=y, logx=True, truncate=False, 
+                seed=42, n_boot=1000, ci=95,
+            ) # Line of best fit
             ax.plot(x, y, ls='', marker='o', ms=5, color='black', alpha=1) # Data
             ax.axhline(y=MCL, color='r', linestyle='--') # MCL
             ax.plot(intersection[0], intersection[1], color='blue', marker='o', ms=10)
@@ -368,16 +452,43 @@ def plot_MCL(self, well_name, analyte_name, year_interval=5, save_dir='plot_MCL'
             predict = num2date(intersection[0]).date()
             l_predict = num2date(intersection_left[0]).date()
             u_predict = num2date(intersection_right[0]).date()
-            ax.annotate(predict, (intersection[0], intersection[1]), xytext=(intersection[0], intersection[1]+1), 
-                        bbox=dict(boxstyle="round", alpha=0.1),ha='center', arrowprops=dict(arrowstyle="->", color='blue'), fontsize=small_fontSize, fontweight='bold')
+            
+            ax.annotate(
+                predict, 
+                (intersection[0], intersection[1]), 
+                xytext=(intersection[0], intersection[1]+1), 
+                bbox=dict(boxstyle="round", alpha=0.1), 
+                ha='center', 
+                arrowprops=dict(arrowstyle="->", color='blue'), 
+                fontsize=small_fontSize, 
+                fontweight='bold',
+            )
+            
             props = dict(boxstyle='round', facecolor='grey', alpha=0.15)
-            ax.text(1.1, 0.5, 'Lower confidence:  {}\n            Prediction:  {}\nUpper confidence:  {}'.format(l_predict, predict, u_predict), transform=ax.transAxes, fontsize=small_fontSize, fontweight='bold', verticalalignment='bottom', bbox=props)
+            ax.text(
+                1.1, 
+                0.5, 
+                # 'Lower confidence: {}\nPrediction: {}\nUpper confidence: {}'.format(l_predict, predict, u_predict), 
+                f"Lower confidence: {l_predict}\nPrediction: {predict}\nUpper confidence: {u_predict}", 
+                transform=ax.transAxes, 
+                fontsize=small_fontSize, 
+                fontweight='bold', 
+                verticalalignment='bottom', 
+                bbox=props,
+            )
 
+            # Save figure
             if not os.path.exists(save_dir):
                 os.makedirs(save_dir)
-            plt.savefig(save_dir + '/' + well_name + '-' + analyte_name +'.png', bbox_inches="tight")
+            
+            plt.savefig(
+                # save_dir + '/' + well_name + '-' + analyte_name +'.png', 
+                f"{save_dir}/{well_name}-{analyte_name}.png", 
+                bbox_inches="tight",
+            )
 
-        except:
-            print('ERROR: Something went wrong')
+        except Exception as e:
+            plots_logger.error('ERROR: Something went wrong')
+            plots_logger.error(e)
             return None
 
