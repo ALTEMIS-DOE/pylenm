389e96fa48f642a3c0b3f90f630e179b5db2abe6
Satyarth Praveen <satyarth@lbl.gov>
2024-09-18 16:00:08 +0530

    Updated disclaimer that `richdem` requires python <= 3.10

--- setup.py
+++ setup.py
@@ -4,13 +4,16 @@ import io
 import os
 import re
 
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
 
 def read(path, encoding='utf-8'):
     path = os.path.join(os.path.dirname(__file__), path)
     with io.open(path, encoding=encoding) as fp:
         return fp.read()
+
 
 def version(path):
     version_file = read(path)
@@ -21,22 +24,23 @@ def version(path):
     raise RuntimeError("Unable to find version string.")
 
 
-setup(name='pylenm',
-      version=version('pylenm/version.py'),
-      description='This package aims to provide machine learning (ML) functions for performing comprehensive soil and groundwater data analysis, and for supporting the establishment of effective long-term monitoring.',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url='https://github.com/ALTEMIS-DOE/pylenm',
-      author='Aurelien Meray',
-      author_email='aurelien.meray@gmail.com',
-      license='MIT',
-      packages=setuptools.find_packages(),
-      install_requires=[
+setup(
+    name='pylenm2',
+    version=version('pylenm2/version.py'),
+    description='This package aims to provide machine learning (ML) functions for performing comprehensive soil and groundwater data analysis, and for supporting the establishment of effective long-term monitoring.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ALTEMIS-DOE/pylenm',
+    author='Aurelien Meray',
+    author_email='aurelien.meray@gmail.com',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    install_requires=[
         'markdown',
         'pandas',
         'openpyxl>=2.6.0',
         'pyproj>=3.0.1',
-        'richdem',
+        # 'richdem',
         'rasterio',
         'numpy',
         'seaborn',
@@ -49,16 +53,16 @@ setup(name='pylenm',
         'supersmoother',
         'ipyleaflet>=0.13.0',
         'ipywidgets>=7.5.0'
-        ],
-      classifiers=[
+    ],
+    classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
-        ],
-      python_requires='>=3.5',
-      zip_safe=False,
-      project_urls={
+    ],
+    python_requires='>=3.5',
+    zip_safe=False,
+    project_urls={
         "Bug Tracker": "https://github.com/ALTEMIS-DOE/pylenm/issues",
         "Documentation": "https://pylenm.readthedocs.io/",
         "Source": "https://github.com/ALTEMIS-DOE/pylenm",
-      },
-      )
+    },
+)
